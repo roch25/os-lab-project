d38cbde7850fee0a8264d0ada0791e8a86fad848
git-roch <rochellemartins25@gmail.com>
2020-06-07 02:30:53 +0530

    srtf algorithm, sort by arrival time

--- project files/scheduling.c
+++ project files/scheduling.c
@@ -5,5 +5,15 @@
 
 void shortest_remaining_time_first(process p[10], int n)
 {
-  
+  process p_tmp;
+  for(int i = 0; i < n; i++) // sorting by arrival time
+  {
+    for(int j = 1; j < n; j++){
+      if(p[i].arrival_time > p[j].arrival_time){
+        p_tmp = p[i];
+        p[i] = p[j];
+        p[j] = p_tmp;
+      }
+    }
+  }
 }
